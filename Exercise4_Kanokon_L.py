048e97167d9da92578c754159a927976ffc2b0e0
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-13 23:34:51 -0500

    Exercise4_Kanokon_L

---
+++ Exercise4_Kanokon_L.py
@@ -0,0 +1,11 @@
+ENG1 = 88
+BUS1 = 84
+COMS1 = 91
+COMP = 86
+
+print("---Your Score---")
+print("Foundation English :",ENG1)
+print("General Business :",BUS1)
+print("Introduction to Computer Systems :",COMS1)
+print("Computer Programming :",COMP)
+
