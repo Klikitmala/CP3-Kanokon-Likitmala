f0df058caf9f8b53b6bbc6c8070f1d20ee88c1a0
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 11:14:41 -0500

    Exercise5_1_Kanokon_L
    Input

--- Exercise5_1_Kanokon_L.py
+++ Exercise5_1_Kanokon_L.py
@@ -1,8 +1,8 @@
-x = int(input())
-y = int(input())
-print(x,"+",y,"=",x + y)
-print(x,"-",y,"=",x - y)
-print(x,"*",y,"=",x * y)
-print(x,"/",y,"=",int(x / y))
+num1 = int(input())
+num2 = int(input())
+print(num1,"+",num2,"=",num1 + num2)
+print(num1,"-",num2,"=",num1 - num2)
+print(num1,"*",num2,"=",num1 * num2)
+print(num1,"/",num2,"=",int(num1 / num2))
 
 
