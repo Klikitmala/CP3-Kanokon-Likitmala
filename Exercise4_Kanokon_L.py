4408a9172d9de22382dbc48c346d5154c3efcc25
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 11:07:35 -0500

    Exercise4_Kanokon_L

--- Exercise4_Kanokon_L.py
+++ Exercise4_Kanokon_L.py
@@ -1,11 +1,17 @@
-ENG1 = 88
-BUS1 = 84
-COMS1 = 91
-COMP = 86
+first_name = "Kanokon"
+last_name = "Likitmala"
+student_ID = "450000000000"
 
+eng1 = 88 #Foundation English
+bus1 = 84 #General Business
+com_sys1 = 91 #Introduction to Computer Sysytem
+com_pro = 86 #Computer Programming
+
+print("KMITL")
+print("Student Name :",first_name, last_name)
+print("Student ID :",student_ID)
 print("---Your Score---")
-print("Foundation English :",ENG1)
-print("General Business :",BUS1)
-print("Introduction to Computer Systems :",COMS1)
-print("Computer Programming :",COMP)
-
+print("Foundation English :",eng1)
+print("General Business :",bus1)
+print("Introduction to Computer Systems :",com_sys1)
+print("Computer Programming :",com_pro)
