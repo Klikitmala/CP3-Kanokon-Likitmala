2ee33a9a6d5368d1465f447077f353cdb0134104
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 23:47:46 -0500

    Exercise8_Kanokon_L
    Nested Condition(Shopping)

--- Exercise8_Kanokon_L.py
+++ Exercise8_Kanokon_L.py
@@ -3,25 +3,29 @@ passwordInput = input("Password :")
 if usernameInput == "Kanokon" and passwordInput == "1234" :
     print("Welcome Y'All !!!!")
     print("<<<<<<  Nanny Shop >>>>>>")
+
     print("1. Squishmello : 13.99 $")
     print("2. Kids Jean : 10.99 $")
     print("3. Kids T-shirt : 4.99 $")
+
+    price1 = 13.99 #1. Squishmello : 13.99 $
+    price2 = 10.99 #2. Kids Jean : 10.99 $
+    price3 = 4.99  #3. Kids T-shirt : 4.99 $
+
     userSelected = int(input("Please Select item's number >>"))
+
     if userSelected == 1:
         print("1. Squishmello : 13.99 $")
-        price1 = 13.99
         quantity1 = int(input("Quantity ="))
         totalPrice1 = quantity1*price1
         print("Total Price :",totalPrice1,"quantity:",quantity1)
     elif userSelected == 2:
         print("2. Kids Jean : 10.99 $")
-        price2 = 10.99
         quantity2 = int(input("Quantity ="))
         totalPrice2 = quantity2*price2
         print("Total Price :",totalPrice2,"quantity:",quantity2)
     elif userSelected == 3:
         print("3. Kids T-shirt : 4.99 $")
-        price3 = 4.99
         quantity3 = int(input("Quantity ="))
         totalPrice3 = quantity3*price3
         print("Total Price :",totalPrice3,"quantity:",quantity3)
