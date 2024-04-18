3b9f48bd96fa6608a35af6ac125af083a10c8383
Klikitmala <likitmalakanokon@yahoo.com>
2024-04-17 23:27:49 -0500

    Exercise8_Kanokon_L
    Nested condition (Shoping)

---
+++ Exercise8_Kanokon_L.py
@@ -0,0 +1,28 @@
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+if usernameInput == "Kanokon" and passwordInput == "1234" :
+    print("Hello Welcome VIP")
+    print("1. Squishmello : 13.99 $")
+    print("2. Kids Jean : 10.99 $")
+    print("3. Kids T-shirt : 4.99 $")
+    userSelected = int(input("Please Select item's number >>"))
+    if userSelected == 1:
+        print("1. Squishmello : 13.99 $")
+        price1 = 13.99
+        quantity1 = int(input("Quantity ="))
+        totalPrice1 = quantity1*price1
+        print("Total Price :",totalPrice1,"quantity:",quantity1)
+    elif userSelected == 2:
+        print("2. Kids Jean : 10.99 $")
+        price2 = 10.99
+        quantity2 = int(input("Quantity ="))
+        totalPrice2 = quantity2*price2
+        print("Total Price :",totalPrice2,"quantity:",quantity2)
+    elif userSelected == 3:
+        print("3. Kids T-shirt : 4.99 $")
+        price3 = 4.99
+        quantity3 = int(input("Quantity ="))
+        totalPrice3 = quantity3*price3
+        print("Total Price :",totalPrice3,"quantity:",quantity3)
+else :
+    print("Could not find your Username or Password, please try again")
