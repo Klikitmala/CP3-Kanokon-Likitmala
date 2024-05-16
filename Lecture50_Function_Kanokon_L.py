c5c3b2d68ee17f648fce082b9f0dff48c325ca5d
Klikitmala <likitmalakanokon@yahoo.com>
2024-05-15 22:39:14 -0500

    Lecture50_Function_Kanokon_L.py

---
+++ Lecture50_Function_Kanokon_L.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(100,25)
+
+def subtraction(x,y):
+    print(x-y)
+subtraction(100,25)
+
+def multiplication(x,y):
+    print(x*y)
+multiplication(100,25)
+
+def devide(x,y):
+    print(x/y)
+devide(100,25)
