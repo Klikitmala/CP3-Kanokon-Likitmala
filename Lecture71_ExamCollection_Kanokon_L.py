f5e338731b9248d1a06ad2814d9e1f5c5f9f6fd3
Klikitmala <likitmalakanokon@yahoo.com>
2024-05-21 00:17:36 -0500

    Lecture71_ExCollection_Kanokon_L

--- Lecture71_ExamCollection_Kanokon_L.py
+++ Lecture71_ExamCollection_Kanokon_L.py
@@ -1,20 +1,21 @@
 menuList = []
 priceList = []
-totalPrice = 0
+
 def showBill():
     print("---- My Food----")
+    totalPrice = 0
     for number in range(len(menuList)):
         print(menuList[number], priceList[number])
-
+        totalPrice = totalPrice + int(priceList[number])
+    print("Total Price : $",totalPrice)
 while True:
-    menuName = input("Plese Enter Menu :")
+    menuName = input("Plese Enter Menu : ")
     if(menuName.lower() == "exit"):
         break
     else:
         menuPrice = input("Price : $")
         menuList.append(menuName)
         priceList.append(menuPrice)
-        totalPrice = totalPrice + int(menuPrice)
 
 showBill()        
-print("Total Price : $",totalPrice)
+
